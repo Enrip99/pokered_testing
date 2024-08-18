314561af6e35c6d4c342c1af74a4ecab542ddd85
enrip99 <enric.1999@hotmail.com>
2024-08-18 23:41:45 +0200

    Arreglar Focus energia

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4629,15 +4629,14 @@ CriticalHitTest:
 	dec hl
 	ld c, [hl]                   ; read move id
 	ld a, [de]
-	bit GETTING_PUMPED, a        ; test for focus energy
-	jr nz, .focusEnergyUsed      ; bug: using focus energy causes a shift to the right instead of left,
-	                             ; resulting in 1/4 the usual crit chance
 	sla b                        ; (effective (base speed/2)*2)
-	jr nc, .noFocusEnergyUsed
+	jr c .critOverflow
+	bit GETTING_PUMPED, a        ; test for focus energy
+	jr z, .noFocusEnergyUsed     ; do not dupe if not using Focus Energy
+	sla b
+	jr nc, .noFocusEnergyUsed    ; if no need to cap, jump
+.critOverflow
 	ld b, $ff                    ; cap at 255/256
-	jr .noFocusEnergyUsed
-.focusEnergyUsed
-	srl b
 .noFocusEnergyUsed
 	ld hl, HighCriticalMoves     ; table of high critical hit moves
 .Loop
